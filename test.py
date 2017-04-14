ca25b1c80de85d97e9ea77949824f145bdcc4cb3
Zach Dischner <zach.dischner@colorado.edu>
2017-04-14 16:40:21 -0600

    Added a decorator to make sure the web service is running to all test cases. Also filtering tests, bad credential test, and added details to artificial antenna asset seeds

--- test.py
+++ test.py
@@ -8,50 +8,100 @@ __maintainer__ = "Zach Dischner"
 __email__      = "zach.dischner@gmail.com"
 __status__     = "Dev"
 __doc__        ="""
-Basic testing script. Definitely not full coverage, but it tests isolated cases
+Basic testing script. Definitely not full coverage, but it tests the main cases. 
+
+Also serves as a nice template for creating requests!
 
 """
 
-import pytest
-import app
 import sys
 import json
 import requests
+import random
 from flask_httpauth import HTTPBasicAuth
 auth=HTTPBasicAuth()
 
 API_URL = "http://127.0.0.1:5000/api/v1.0"
 
+def is_running(f, *args, **kwargs):
+    def wrapped():
+        try:
+            requests.get(API_URL)
+            return f(*args, **kwargs)
+        except requests.ConnectionError:
+            assert 0==1, f"{API_URL} is not reachable!"
+    return wrapped
+
+@is_running
 def test_dropall(user="sudo", password="rm/*"):
     resp = requests.delete(f'{API_URL}/assets/',headers={'Content-Type': 'application/json'}, auth=(user,password) )
     assert resp.ok, f"Problem! {resp.json()}"
     return resp
 
+@is_running
 def test_getall():
     resp = requests.get(f'{API_URL}/assets/',headers={'Content-Type': 'application/json'})
     assert resp.ok, f"Problem! {resp.json()}"
     return resp
 
-def test_add_assets(num=3, user="admin", password=""):
+@is_running
+def test_add_assets(num=10, user="admin", password=""):
     for num in range(num):
+        # Add a few satellites
+
         resp = requests.post(f'{API_URL}/assets/sc-test-{num}',
                              headers={'Content-Type': 'application/json'},
                              json={"asset_type":"satellite", "asset_class":"dove"},
                              auth=(user, password))
         assert resp.ok, f"Problem! {resp.json()}"
 
+        ## Now add some antennas with extra details!
         resp = requests.post(f'{API_URL}/assets/ant-test-{num}',
                              headers={'Content-Type': 'application/json'},
-                             json={"asset_type": "antenna", "asset_class": "dish"},
+                             json={"asset_type": "antenna", "asset_class": "dish",
+                                   "asset_details":{
+                                        "diameter": random.random()*100,
+                                        "randome": random.randint(0,1) }
+                                   },
                              auth=(user, password))
         assert resp.ok, f"Problem! {resp.json()}"
     return resp
 
+@is_running
+def test_bad_credentials():
+    resp = requests.post(f'{API_URL}/assets/sc-test-foo',
+                         headers={'Content-Type': 'application/json'},
+                         json={"asset_type": "satellite", "asset_class": "dove"},
+                         auth=("foo_user", "foo_password"))
+    assert resp.status_code == 401, f"User with wrong credentials was allowed to POST an asset! {resp.json()}"
+    return resp
+
+@is_running
+def test_filtered():
+    resp = requests.get(f'{API_URL}/assets/',
+                        headers={'Content-Type': 'application/json'},
+                        json={"asset_type":"antenna"})
+    assert resp.ok, f"Problem! {resp.json()}"
+    assets = resp.json()['assets']
+    assert False not in ["antenna"==asset['type'] for asset in assets], f"Filtering assets for just antennas didn't work! Returned assets: {assets}"
+
+    resp = requests.get(f'{API_URL}/assets/',
+                        headers={'Content-Type': 'application/json'},
+                        json={"asset_class": "dove"})
+    assert resp.ok, f"Problem! {resp.json()}"
+    assets = resp.json()['assets']
+    assert False not in ["dove" == asset['class'] for asset in assets], f"Filtering assets for just dove types didn't work! Returned assets: {assets}"
+
+    return resp
 
 def main():
+    print("Running all test cases for the Asset API")
+    if is_running() is False:
+        print(f"{API_URL} not reachable, cannot run Asset API tests")
+        return -1
 
     ## Prelim checks - reachable and empty asset datastore
-    print("Making sure service is reachable and taking requests")
+    print("Getting existing Assets")
     resp = test_getall()
     print(f"All Assets: {json.dumps(resp.json(),indent=2)}")
 
@@ -70,7 +120,15 @@ def main():
     resp = test_add_assets()
     print(f"Response from adding the last asset : {json.dumps(resp.json(),indent=2)}")
 
+    ## Test bad credentials
+    print("\n\nTesting a user with bad credentials")
+    resp = test_bad_credentials()
+    print(f"Response from trying to add bad credentials : {json.dumps(resp.json(),indent=2)}")
 
+    ## Test filtering query
+    print("\n\nTesting out asset filtering query")
+    resp = test_filtered()()
+    print(f"Response from filtering on 'dove' type assets {json.dumps(resp.json(),indent=2)}")
     return 0
 
 if __name__ == "__main__":
